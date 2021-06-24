b000a000386e12b71335baac5e05af3b907c27b1
YuSiyuan <yusiyuan05@meituan.com>
2021-06-24 15:26:20 +0800

    AFNetworking GET request

--- Network/ViewController.m
+++ Network/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import <AFNetworking/AFNetworking.h>
 
 @interface ViewController ()
 
@@ -15,8 +16,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self getRequest];
 }
 
+- (void)getRequest
+{
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    NSDictionary *dict = @{
+        @"username":@"sy",
+        @"pwd":@"123"
+    };
+    [manager GET:@"https://www.baidu.com/" parameters:dict success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            // task:可以通过task拿到响应头
+            //responseObject:请求成功返回的响应结果（AFN内部已经把响应体转换为OC对象，通常是字典或数组)
+            NSLog(@"responseObject---%@", [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding]);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            NSLog(@"error---%@", error);
+        }];
+}
 
 @end
---
+++ Podfile
@@ -0,0 +1,5 @@
+target "Network" do
+    source 'https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git'
+    platform :ios, '7.0'
+    pod 'AFNetworking', '~> 2.5'
+end
